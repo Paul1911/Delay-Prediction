2421208a555a97a26fbdcc52de18d88e0837fd10
Paul Friedrich <paul.friedrich@iubh.de>
2022-06-19 16:39:38 +0200

    a lot of changes and comparisons to Hinnerks results

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -28,15 +28,16 @@ def create_grounddelay(df):
 
 # Block time delay in minutes
 def create_blockdelay(df):
-    df['block_delay'] = (df['actual_block_time'] - (
-        (df['arr_sched_time']-df['dep_sched_time']).dt.total_seconds()/60) # this is just scheduled_block_time without adding the column
-    )
+    #df['block_delay'] = (df['actual_block_time'] - (
+    #    (df['arr_sched_time']-df['dep_sched_time']).dt.total_seconds()/60) # this is just scheduled_block_time without adding the column
+    #)
+    df['block_delay'] = (df['m_onblockdt']-df['arr_sched_time']).dt.total_seconds()/60
 
 # Rotational mismatch indicator
 def create_rotmismatch(df):
     '''Takes a dataframe and adds a rotation mismatch flag if the arrival airport is not equal to the next departure airport'''
     df['rot_mismatch'] = np.where(
-        df['ac_registration_x'].shift(-1).eq(df['ac_registration_x']) == True, # Where the registration is the same as the row above
+        df['ac_registration_x'].shift(-1).eq(df['ac_registration_x']) == True, # Where the registration is the same as the row below
         ~df['dep_ap_sched'].shift(-1).eq(df['arr_ap_sched']), # Check whether current dep ap is the same as next arr. ap | Tilde negates the bool value
         False # Set false in case the registrations are not equal, as then a new ac rotation pattern follows
     )
